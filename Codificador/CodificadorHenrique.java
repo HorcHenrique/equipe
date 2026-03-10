05b08534222fa41a147ec1201b079e6bca2bb499
Henrique Horch <hhorchenrique@gmail.com>
2026-03-10 08:27:13 -0300

    start project

---
+++ Codificador/CodificadorHenrique.java
@@ -0,0 +1,33 @@
+
+import java.util.StringBuilder;
+
+
+public class CodificadorHenrique implements Codificador {
+
+public void codifica(String str) {
+    StringBuilder word = new StringBuilder();
+
+   for (char c : str.toCharArray) {
+
+   if(word.toCharArray.lenght % 3 == 0){
+    word.append("#d&");
+    wfwefvw
+
+   }
+
+
+   }
+
+
+
+
+}
+
+
+
+
+
+
+
+}
+
